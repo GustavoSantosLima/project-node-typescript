1a5a008842a74164312b55cfb45ef291f42517f6
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2024-05-04 13:21:22 -0300

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,14 +1,20 @@
 export function api(url: string, options: RequestInit = {}) {
-  return fetch(`http://localhost:3333${url}`, {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    ...options
-  });
+  return fetch(
+    `https://project-node-typescript-backend.vercel.app/customer${url}`,
+    {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      ...options
+    }
+  );
 }
 
 export function apiUpload(url: string, options: RequestInit = {}) {
-  return fetch(`http://localhost:3333${url}`, {
-    ...options
-  });
+  return fetch(
+    `https://project-node-typescript-backend.vercel.app/customer${url}`,
+    {
+      ...options
+    }
+  );
 }
